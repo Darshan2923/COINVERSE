03d3d0df4e416989d7848bfcbd501eddb946b968
Darshan <darshanpatel2923@gmail.com>
2023-07-04 20:15:15 +0530

    Added smooth scrolling(best experience for me)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,18 @@ import '../styles/Navbar.css'
 
 
 function Navbar() {
+
     return (
         <div>
             <div className="header">
                 <div className='logo-brand'>
-                    <a className='logo-brand-link' href="#home">COINVERSE</a>
+                    <a className='logo-brand-link' href="#home" >COINVERSE</a>
                 </div>
                 <ul className='structure-header'>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#market">Market</a></li>
-                    <li><a href="#choose">Choose Us</a></li>
-                    <li><a href="#join">Join Us</a></li>
+                    <li><a href="#home"  >Home</a></li>
+                    <li><a href="#market"  >Market</a></li>
+                    <li><a href="#choose"  >Choose Us</a></li>
+                    <li><a href="#join"  >Join Us</a></li>
                     <div className="nav-socials">
                         <a rel="noreferrer" href="https://twitter.com/axion_network" target='_blank'><i className="nav-socials fa-brands fa-twitter"></i></a>
                         <a rel="noreferrer" href="https://discord.com/invite/money" target='_blank'><i className="nav-socials fa-brands fa-discord"></i></a>
