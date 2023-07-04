6e65de60a840a04a3b5448c423d37b061160d973
Darshan <darshanpatel2923@gmail.com>
2023-07-04 19:55:34 +0530

    Added JoinUs only API work left

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import '../styles/Join_footer.css'
+import logo1 from '../styles/bitcoin-floating.png'
+import logo2 from '../styles/ethereum-floating.png'
 
 function Join() {
     return (
         <section id='join'>
             <div className='join-us-discord'>
                 <div className="join-title">
-                    <h1>Join Us Via <span>Discord</span></h1>
+                    <img className='eth-float2' src={logo2} alt="" />
+                    <h1>Join Us Via <br /> <span>Discord</span></h1>
+                    <img className='btc-float2' src={logo1} alt="" />
                 </div>
-                <div className="join-subtitle">Invest and manage all your crypto at one place</div>
+                <div className="join-subtitle">Invest and manage all your crypto at <br /> one place</div>
             </div>
-            <div className="button-join"><input type="button" value={"Join Via Discord"} /></div>
+            <div className="button-join"><button className='link-button'><a rel="noreferrer" href="https://discord.com/invite/money" target='_blank'>Join Via Discord</a></button></div>
             <footer>
                 <div className="footer-content">
                     <div className="footer-content__socials">
-                        <i className="fa-brands fa-twitter"></i>
-                        <i className="fa-brands fa-discord"></i>
-                        <i className="fa-brands fa-facebook"></i>
-                        <i className="fa-brands fa-youtube"></i>
+                        <a rel="noreferrer" href="https://twitter.com/axion_network" target='_blank'><i className="fa-brands fa-twitter"></i></a>
+                        <a rel="noreferrer" href="https://discord.com/invite/money" target='_blank'><i className="fa-brands fa-discord"></i></a>
+                        <a rel="noreferrer" href="https://www.facebook.com/AxionCryptoCD/" target='_blank'><i className="fa-brands fa-facebook"></i></a>
+                        <a rel="noreferrer" href="https://www.youtube.com/channel/UCfA9GNOsiYfdoEEXgOvAtYQ" target='_blank'><i className="fa-brands fa-youtube"></i></a>
+
                     </div>
                     <div className="footer-content__text">
-                        <p>Privacy</p>
+                        <p className='privacy'>Privacy</p>
                         <p>Terms of Use</p>
                     </div>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import '../styles/Navbar.css'
 
+
 function Navbar() {
     return (
         <div>
             <div className="header">
                 <div className='logo-brand'>
-                    <a href="#home">COINVERSE</a>
+                    <a className='logo-brand-link' href="#home">COINVERSE</a>
                 </div>
                 <ul className='structure-header'>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#market">Market</a></li>
                     <li><a href="#choose">Choose Us</a></li>
                     <li><a href="#join">Join Us</a></li>
+                    <div className="nav-socials">
+                        <a rel="noreferrer" href="https://twitter.com/axion_network" target='_blank'><i className="nav-socials fa-brands fa-twitter"></i></a>
+                        <a rel="noreferrer" href="https://discord.com/invite/money" target='_blank'><i className="nav-socials fa-brands fa-discord"></i></a>
+                    </div>
                 </ul>
             </div>
         </div>
