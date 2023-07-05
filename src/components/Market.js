8d5517a8383760f7aaf3104a19076414f86321ff
Darshan <darshanpatel2923@gmail.com>
2023-07-05 21:10:10 +0530

    API limit exceeded so can't do anything more...

--- src/components/Market.js
+++ src/components/Market.js
@@ -4,7 +4,7 @@ import '../styles/Market.css'
 
 function Market() {
     const [data, setData] = useState([]);
-    const [coinsLoad, setCoinsLoad] = useState(true);
+    // const [coinsLoad, setCoinsLoad] = useState(true);
 
     const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false`;
     useEffect(() => {
@@ -17,7 +17,26 @@ function Market() {
     }, [url]);
 
     const apiElements = data.map(item => {
+        return (
+            <div className="market-container__row-elements">
+                <span className='market_coins_img'>
+                    <img src={item.image} alt="item.name" />{item.name}
+                </span>
+                <p>{"$ " + item.current_price.toFixed(2)}</p>
+                <p
+                    className={
+                        "slider-coin__price " +
+                        (item.price_change_percentage_24h >= 0
+                            ? { style: { color: "green" } }
+                            : { style: { color: "red" } })
+                    }
+                >
+                    {item.price_change_percentage_24h.toFixed(2) + " %"}
+                </p>
+                <p>{"$ " + (item.market_cap)}</p>
 
+            </div>
+        )
     })
 
     return (
@@ -35,6 +54,7 @@ function Market() {
                     </div>
                     <div className="market-content__coin-list__row">
                         {apiElements}
+                        {console.log(data)}
                     </div>
                 </div>
 
