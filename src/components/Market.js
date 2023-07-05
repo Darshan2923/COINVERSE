eeec35acc65368090d36af8f36dcfaa6f3f750b8
Darshan <darshanpatel2923@gmail.com>
2023-07-05 20:46:39 +0530

    Started Market section and fetched a little data.

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import '../styles/Market.css'
 
 function Market() {
+    const [data, setData] = useState([]);
+    const [coinsLoad, setCoinsLoad] = useState(true);
+
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false`;
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url);
+            const parseData = await response.json();
+            setData(parseData);
+        }
+        fetchData();
+    }, [url]);
+
+    const apiElements = data.map(item => {
+
+    })
+
     return (
-        <div>Market</div>
+        <section id='market'>
+            <div className='market-container'>
+                <div className="market-title">
+                    <h3>Market Update</h3>
+                </div>
+                <div className="market-content__coin-list">
+                    <div className="market-content__coin-list__top">
+                        <p>Coin</p>
+                        <p>Price</p>
+                        <p>24h Change</p>
+                        <p>Market Change</p>
+                    </div>
+                    <div className="market-content__coin-list__row">
+                        {apiElements}
+                    </div>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
